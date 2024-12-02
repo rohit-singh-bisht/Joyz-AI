298ff373216fd66189b852e5df1309a3a823d303
rohit-singh-bisht <thersbisht@gmail.com>
2024-12-02 21:41:17 +0530

    new header footer fixes

--- layout/footer.js
+++ layout/footer.js
@@ -5,9 +5,13 @@ class AppFooter extends HTMLElement {
             <div class="container">
                 <div class="row">
                 <div class="col-12">
-                    <h2 class="section_subtitle">Vision</h2>
+                    <h2 class="section_subtitle">
+                        <a href="#">Vision</a>
+                    </h2>
 
-                    <div class="footer__social"></div>
+                    <div class="footer__social">
+                        <app-social></app-social>
+                    </div>
 
                     <div class="footer__copymark">
                     &copy; 2024, JoyzAI. All Rights Reserved.
