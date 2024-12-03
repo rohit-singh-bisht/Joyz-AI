8b27194933ac00cb72f0b0996f758e18bd652b4f
rohit-singh-bisht <thersbisht@gmail.com>
2024-12-04 00:12:52 +0530

    home page modal finished

--- survey-script.js
+++ survey-script.js
@@ -94,8 +94,8 @@ function createSurveyForm(urlValue) {
     pages: [
       {
         name: "page1",
-        title: "Simple Setup",
-        description: `<ol class='modal__steps'><li>Enter website URL</li><li>Share basic details</li><li>Test the chatbot</li><li>Add code to site</li></ol>`,
+        description:
+          "<h2 class='modal__title'>Simple Setup</h2> <ol class='modal__steps'><li>Enter website URL</li><li>Share basic details</li><li>Test the chatbot</li><li>Add code to site</li></ol>",
         elements: [
           {
             type: "text",
@@ -118,14 +118,18 @@ function createSurveyForm(urlValue) {
       },
       {
         name: "page2",
-        navigationButtonsVisibility: "show",
+        description: `<h2 class='modal__tit'>We are building your chatbot...</h2> <p class='modal__sub'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" viewBox="0 0 16 15" fill="none">
+        <path d="M11.3438 5.50047C10.9688 5.50047 10.6562 5.81297 10.6562 6.18797C10.6562 6.53172 10.9688 6.84422 11.3438 6.84422H15.3125C15.6875 6.84422 16 6.53172 16 6.18797V2.18797C16 1.81297 15.6875 1.50047 15.3125 1.50047C14.9688 1.50047 14.6562 1.81297 14.6562 2.18797V5.50047H11.3438ZM1.34375 9.50047H4.65625C5.03125 9.50047 5.34375 9.21922 5.34375 8.84422C5.34375 8.46922 5.03125 8.15672 4.65625 8.15672H0.6875C0.3125 8.15672 0 8.46922 0 8.84422V12.8442C0 13.188 0.3125 13.5005 0.6875 13.5005C1.03125 13.5005 1.34375 13.188 1.34375 12.8442V9.50047ZM2.96875 5.71922C3.59375 4.00047 5.03125 2.71922 6.8125 2.31297C8.59375 1.90672 10.4688 2.43797 11.75 3.75047L14.875 6.65672C15.125 6.90672 15.5625 6.90672 15.8125 6.62547C16.0625 6.37547 16.0625 5.93797 15.7812 5.68797L12.6875 2.78172C11.0625 1.18797 8.75 0.500466 6.53125 1.00047C4.3125 1.50047 2.46875 3.12547 1.71875 5.28172C1.59375 5.62547 1.78125 6.00047 2.125 6.12547C2.46875 6.25047 2.84375 6.06297 2.96875 5.71922ZM0.21875 9.31297L3.3125 12.2192C4.9375 13.8442 7.25 14.5005 9.46875 14.0005C11.7188 13.5005 13.5312 11.8755 14.2812 9.71922C14.4062 9.37547 14.2188 9.00047 13.875 8.87547C13.5312 8.75047 13.1562 8.93797 13.0312 9.28172C12.4062 11.0005 10.9688 12.2817 9.1875 12.688C7.40625 13.0942 5.53125 12.563 4.25 11.2817L1.125 8.34422C0.875 8.09422 0.4375 8.12547 0.1875 8.37547C-0.0625 8.65672 -0.0625 9.06297 0.21875 9.31297Z" fill="#2E2E2E"/>
+        </svg> Gathering data</p><div class="chatbot__basic__details">Meanwhile share some basic details to complete your chatbot...</div>`,
         elements: [
           {
             type: "dropdown",
-            name: "queriesPerDay",
-            title: "How many support queries do you get per day?",
+            name: "repsCount",
+            title: "How many support reps do you have?",
             hideNumber: true,
             isRequired: true,
+            optionsCaption: "Choose...",
+            allowClear: false,
             choices: [
               {
                 value: "lessThan10",
@@ -145,6 +149,7 @@ function createSurveyForm(urlValue) {
     pageNextText: "Create Your Chatbot Now",
     completeText: "Next",
     questionErrorLocation: "bottom",
+    showPrevButton: false,
   };
 
   window.survey = new Survey.Model(surveyJSON);
@@ -233,6 +238,18 @@ function createSurveyForm(urlValue) {
         .classList.add(`sv-footer__next-btn_${page.page.name}`);
       previousClass = `sv-footer__next-btn_${page.page.name}`;
     }
+  });
+
+  survey.onOpenDropdownMenu.add((_, options) => {
+    options.menuType = "dropdown";
+  });
+
+  const converter = markdownit({
+    html: true,
+  });
+  survey.onTextMarkdown.add((_, options) => {
+    let str = converter.renderInline(options.text);
+    options.html = str;
   });
 
   survey.clearInvisibleValues = "onHidden";
